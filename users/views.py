bef61c2db2438a899c346ad9c96aa530b6c5173f
Kuldeep2001 <ks2596050@gmail.com>
2022-07-15 19:06:49 +0530

    Initial Setup

---
+++ users/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'users/index.html')
+
+def register(request):
+    return render(request, 'users/index.html')
+
+def login(request):
+    return render(request, 'users/login.html')
